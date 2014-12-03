fca1b7f6a2ab21a2f12c844b24cef5bfad10bb9e
zsergiuz93 <sergiuzdrobau@gmail.com>
2014-12-03 15:54:36 +0200

    added mustache

--- js/myscript.js
+++ js/myscript.js
@@ -1,16 +1,35 @@
-$ = jQuery;
 var nr_of_reg = 0; var nr_max = 0; nr_max = localStorage.getItem('nr_max');
-if (typeof nr_max === 'undefined' || nr_max == null || nr_max == 0){
+var allData = new Object;  
+if (typeof nr_max === 'undefined' || nr_max == null || nr_max == 0)
+{
 nr_max = 0;
-id = nr_max;}
+id = nr_max;
+}
+
+function mustacheDo(){
+allData = localStorage.getItem('allData');
+allData = JSON.parse(allData);
+var tmpl = document.getElementById("helloTmpl").innerHTML;
+var html = Mustache.to_html(tmpl, allData);
+var box = document.getElementById("users");
+box.innerHTML = html;
+}
+function showSumms(a,b,c){
+        var added_div = document.createElement("div");
+        added_div.style.fontSize = "14px";
+        added_div.id = "added_div";
+        added_div.innerHTML = '<p>#Of Market Launches: '+a+'</p> <p>Regional Total Amount of Estimated Sales Potential: '+b+'</p> <p>Global Total Amount of Estimated Sales Potential: '+parseFloat(c).toFixed(2)+'</p>';
+        document.body.appendChild(added_div);
+}
+
     function edit_row(id)
     {
       var dialog3, form,
       title = $( "#title3" ),
       brand = $( "#brand3" ),
-      win = $( "#win3" ).val();
-      timeframe = $( "#timeframe3" ).val();
-      timeframeyear = $( "#timeframeyear3" ).val();
+      win = $( "#win3" );
+      timeframe = $( "#timeframe3" );
+      timeframeyear = $( "#timeframeyear3" );
       sales = $( "#sales3" ),
       allFields = $( [] ).add( title ).add( brand ).add( sales ),
       tips = $( ".validateTips" );
@@ -63,48 +82,36 @@ function checkRegexp( o, regexp, n ) {
         var sales2 = parseInt(sales.val())/1.1;
         var suma1 = suma1+parseInt(sales.val());
         var suma2 = suma1/1.1;
-      localStorage.setItem('title['+id+']', title.val());
-      localStorage.setItem('brand['+id+']', brand.val());
-      localStorage.setItem('timeframe['+id+']', timeframe);
-      localStorage.setItem('timeframeyear['+id+']', timeframeyear);
-      localStorage.setItem('sales['+id+']', parseInt(sales.val()));
+
+          users = localStorage.getItem('users');
+          users = JSON.parse(users);
+          users['id'+id] = id; users['title'+id] = title.val(); users['brand'+id] = brand.val(); users['timeframe'+id] = timeframe.val(); users['win'+id] = win.val(); users['timeframeyear'+id] = timeframeyear.val(); users['sales'+id] = parseInt(sales.val());
+          localStorage.setItem('users', JSON.stringify(users));
+
 var suma_sel1 = 0;
+var data_to_array = [];
   for (i=1;i<=nr_max;i++)
   {
     var element_to_delete = document.getElementById("added_div");
         if (element_to_delete){element_to_delete.parentNode.removeChild(element_to_delete);}
-        title_sel = localStorage.getItem('title['+i+']'); 
+
+        users = localStorage.getItem('users');
+          users = JSON.parse(users);
+        title_sel = users['title'+i]; 
                if (typeof title_sel == 'string'){
-        brand_sel = localStorage.getItem('brand['+i+']');
-        timeframe_sel = localStorage.getItem('timeframe['+i+']');
-        timeframeyear_sel = localStorage.getItem('timeframeyear['+i+']');
-        sales_sel = localStorage.getItem('sales['+i+']');
-        sales_sel = parseInt(sales_sel);
+
+
+data_to_array.push({"0": users['id'+i], "1" : users['title'+i], "2" : users['brand'+i], "3": users['sales'+i], "4" : (parseInt(users['sales'+i]) / 1.1).toFixed(2), "5" : users['timeframe'+i], "6" : users['timeframeyear'+i]});
+      allData = {"getData" : data_to_array};
+        localStorage.setItem('allData', JSON.stringify(allData));
+        mustacheDo();
+        sales_sel = users['sales'+i];
         sales2_sel = sales_sel/1.1;
         suma_sel1 = parseInt(suma_sel1 + sales_sel);
         suma_sel2 = parseInt(suma_sel1/1.1);
-
-                  $( "#users tbody" ).append( "<tr>" +
-          "<td>ML" + i + "</td>" +
-          "<td>" + title_sel + "</td>" +
-          "<td>" + brand_sel + "</td>" +
-          "<td>" + timeframe_sel + " " +timeframeyear_sel + "</td>" +
-          "<td>" + parseInt(sales_sel) + "</td>" +
-          "<td>" + parseFloat(sales2_sel).toFixed(2) + "</td>" +
-          "<td>To Be Launched</td>" +
-          "<td><img style='cursor:pointer;' onclick='edit_row("+i+")' width='16px' height='16px' id='edit"+i+"' src='css/images/pencil.png'>  <img style='cursor:pointer;' onclick='delete_row("+i+")' width='16px' height='16px' id='delete"+i+"' src='css/images/delete.jpg'></td>" +
-        "</tr>" );
-        var added_div = document.createElement("div");
-        added_div.style.fontSize = "14px";
-        added_div.id = "added_div";
-        added_div.innerHTML = '<p>#Of Market Launches: '+nr_of_reg+'</p> <p>Regional Total Amount of Estimated Sales Potential: '+suma_sel1+'</p> <p>Global Total Amount of Estimated Sales Potential: '+parseFloat(suma_sel2).toFixed(2)+'</p>';
-        document.body.appendChild(added_div);
+         showSumms(nr_of_reg, suma_sel1, suma_sel2);  
         }else {
-        var added_div = document.createElement("div");
-        added_div.style.fontSize = "14px";
-        added_div.id = "added_div";
-        added_div.innerHTML = '<p>#Of Market Launches: '+nr_of_reg+'</p> <p>Regional Total Amount of Estimated Sales Potential: '+suma_sel1+'</p> <p>Global Total Amount of Estimated Sales Potential: '+parseFloat(suma_sel2).toFixed(2)+'</p>';
-        document.body.appendChild(added_div);      
+        showSumms(nr_of_reg, suma_sel1, suma_sel2);    
         }
   }  dialog3.dialog( "close" );
       }      return valid;
@@ -115,61 +122,58 @@ var suma_sel1 = 0;
       }
     }); //dialog3 close
     var suma_sel1, suma_sel2; 
-    title_sel = localStorage.getItem('title['+id+']');
-    brand_sel = localStorage.getItem('brand['+id+']');
-    timeframe_sel = localStorage.getItem('timeframe['+id+']');
-    timeframeyear_sel = localStorage.getItem('timeframeyear['+id+']');
-    sales_sel = localStorage.getItem('sales['+id+']');
+          users = localStorage.getItem('users');
+          users = JSON.parse(users);
+    title_sel = users['title'+id];
+    brand_sel = users['brand'+id];
+    timeframe_sel = users['timeframe'+id];
+    win_sel = users['win'+id];
+    timeframeyear_sel = users['timeframeyear'+id];
+    sales_sel = users['sales'+id];
     $('#title3').val(title_sel);
     $('#brand3').val(brand_sel);
     $('#sales3').val(sales_sel);
     $('#timeframe3').val(timeframe_sel);
     $('#timeframeyear3').val(timeframeyear_sel);
+    $('#win3').val(win_sel);
       dialog3.dialog( "open" );
     }
+
 function delete_row(id){
+ 
   $( "#delete"+id ).on( "click", function() {
       function delete_row1(id){
-        localStorage.removeItem('id['+id+']'); localStorage.removeItem('title['+id+']'); localStorage.removeItem('brand['+id+']');  localStorage.removeItem('timeframe['+id+']');  localStorage.removeItem('timeframeyear['+id+']'); localStorage.removeItem('sales['+id+']'); localStorage.removeItem('sales2['+id+']');
-var title_sel = ""; var brand_sel = ""; var timeframe_sel = ""; var timeframeyear_sel = ""; var sales_sel = 0; var sales2_sel = 0; var suma_sel1 = 0; var suma_sel2 = 0; nr_of_reg = 0;
+      var title_sel = ""; var brand_sel = ""; var timeframe_sel = ""; var timeframe_sel = ""; var timeframeyear_sel = ""; var sales_sel = 0; var sales2_sel = 0; var suma_sel1 = 0; var suma_sel2 = 0; nr_of_reg = 0;       
+        users = localStorage.getItem('users');
+          users = JSON.parse(users);
+         delete users['id'+id]; delete users['title'+id]; delete users['brand'+id]; delete users['timeframe'+id]; delete users['win'+id]; delete users['timeframeyear'+id]; delete users['sales'+id];
+         localStorage.setItem('users', JSON.stringify(users));
+         users = localStorage.getItem('users');
+        users = JSON.parse(users);  
+
+var data_to_array = [];
+
   for (i=1;i<=nr_max;i++)
   {
     id = nr_max; suma1 = suma_sel1; suma2 = sales2_sel;
     var element_to_delete = document.getElementById("added_div");
         if (element_to_delete){element_to_delete.parentNode.removeChild(element_to_delete);}
-        title_sel = localStorage.getItem('title['+i+']'); 
+        title_sel = users['title'+i]; 
                if (typeof title_sel == 'string'){
-        title_sel = localStorage.getItem('title['+i+']'); 
-        brand_sel = localStorage.getItem('brand['+i+']');
-        timeframe_sel = localStorage.getItem('timeframe['+i+']');
-        timeframeyear_sel = localStorage.getItem('timeframeyear['+i+']');
-        sales_sel = localStorage.getItem('sales['+i+']');
+data_to_array.push({"0": users['id'+i], "1" : users['title'+i], "2" : users['brand'+i], "3": users['sales'+i], "4" : (parseInt(users['sales'+i]) / 1.1).toFixed(2), "5" : users['timeframe'+i], "6" : users['timeframeyear'+i]});
+      allData = {"getData" : data_to_array};
+          localStorage.setItem('allData', JSON.stringify(allData));
+          mustacheDo();
+        sales_sel = users['sales'+i];
         sales_sel = parseInt(sales_sel);
         sales2_sel = sales_sel/1.1;
         suma_sel1 = suma_sel1 + sales_sel;
         suma_sel2 = suma_sel1/1.1;
-                  $( "#users tbody" ).append( "<tr>" +
-          "<td>ML" + i + "</td>" +
-          "<td>" + title_sel + "</td>" +
-          "<td>" + brand_sel + "</td>" +
-          "<td>" + timeframe_sel + " " +timeframeyear_sel + "</td>" +
-          "<td>" + parseInt(sales_sel) + "</td>" +
-          "<td>" + parseFloat(sales2_sel).toFixed(2) + "</td>" +
-          "<td>To Be Launched</td>" +
-          "<td><img style='cursor:pointer;' onclick='edit_row("+i+")' width='16px' height='16px' id='edit"+i+"' src='css/images/pencil.png'>  <img style='cursor:pointer;' onclick='delete_row("+i+")' width='16px' height='16px' id='delete"+i+"' src='css/images/delete.jpg'></td>" +
-        "</tr>" );
                     ++nr_of_reg;
-        var added_div = document.createElement("div");
-        added_div.style.fontSize = "14px";
-        added_div.id = "added_div";
-        added_div.innerHTML = '<p>#Of Market Launches: '+nr_of_reg+'</p> <p>Regional Total Amount of Estimated Sales Potential: '+suma_sel1+'</p> <p>Global Total Amount of Estimated Sales Potential: '+parseFloat(suma_sel2).toFixed(2)+'</p>';
-        document.body.appendChild(added_div);
-        }else {
-        var added_div = document.createElement("div");
-        added_div.style.fontSize = "14px";
-        added_div.id = "added_div";
-        added_div.innerHTML = '<p>#Of Market Launches: '+nr_of_reg+'</p> <p>Regional Total Amount of Estimated Sales Potential: '+suma_sel1+'</p> <p>Global Total Amount of Estimated Sales Potential: '+parseFloat(suma_sel2).toFixed(2)+'</p>';
-        document.body.appendChild(added_div);      
+showSumms(nr_of_reg, suma_sel1, suma_sel2);  
+        }
+        else {
+showSumms(nr_of_reg, suma_sel1, suma_sel2);     
         }
   } 
       }
@@ -200,15 +204,23 @@ if (nr_max<1){
          var id=0;
     var suma1 = 0;
     var suma2 = 0;
- } else
- { var title_sel = "";
-var brand_sel = "";
+ } 
+ else { var title_sel = "";
+var brand_sel = ""; var win_sel = "";
 var timeframe_sel = "";
 var timeframeyear_sel = "";
 var sales_sel = 0;
 var sales2_sel = 0;
 var suma_sel1 = 0;
-var suma_sel2 = 0;
+var suma_sel2 = 0;   
+
+users = localStorage.getItem('users');
+users = JSON.parse(users);  
+
+var data_to_array = [];
+
+
+
   for (i=1;i<=nr_max;i++)
   {
     id = nr_max;
@@ -219,50 +231,35 @@ var suma_sel2 = 0;
         {
           element_to_delete.parentNode.removeChild(element_to_delete);
         }
-        title_sel = localStorage.getItem('title['+i+']'); 
+
+        title_sel = users['title'+i]; 
                if (typeof title_sel == 'string'){
-        title_sel = localStorage.getItem('title['+i+']'); 
-        brand_sel = localStorage.getItem('brand['+i+']');
-        timeframe_sel = localStorage.getItem('timeframe['+i+']');
-        timeframeyear_sel = localStorage.getItem('timeframeyear['+i+']');
-        sales_sel = localStorage.getItem('sales['+i+']');
+      data_to_array.push({"0": users['id'+i], "1" : users['title'+i], "2" : users['brand'+i], "3": users['sales'+i], "4" : (parseInt(users['sales'+i]) / 1.1).toFixed(2), "5" : users['timeframe'+i], "6" : users['timeframeyear'+i]});
+      allData = {"getData" : data_to_array};
+          localStorage.setItem('allData', JSON.stringify(allData));
+
+        sales_sel = users['sales'+i];
         sales_sel = parseInt(sales_sel);
         sales2_sel = sales_sel/1.1;
         suma_sel1 = suma_sel1  + sales_sel;
         suma_sel2 = suma_sel1/1.1;
-                  $( "#users tbody" ).append( "<tr>" +
-          "<td>ML" + i + "</td>" +
-          "<td>" + title_sel + "</td>" +
-          "<td>" + brand_sel + "</td>" +
-          "<td>" + timeframe_sel + " " +timeframeyear_sel + "</td>" +
-          "<td>" + parseInt(sales_sel) + "</td>" +
-          "<td>" + parseFloat(sales2_sel).toFixed(2) + "</td>" +
-          "<td>To Be Launched</td>" +
-          "<td><img style='cursor:pointer;' onclick='edit_row("+i+")' width='16px' height='16px' id='edit"+i+"' src='css/images/pencil.png'>  <img style='cursor:pointer;' onclick='delete_row("+i+")' width='16px' height='16px' id='delete"+i+"' src='css/images/delete.jpg'></td>" +
-        "</tr>" );
+
        ++nr_of_reg;
-        var added_div = document.createElement("div");
-        added_div.style.fontSize = "14px";
-        added_div.id = "added_div";
-        added_div.innerHTML = '<p>#Of Market Launches: '+nr_of_reg+'</p> <p>Regional Total Amount of Estimated Sales Potential: '+suma_sel1+'</p> <p>Global Total Amount of Estimated Sales Potential: '+parseFloat(suma_sel2).toFixed(2)+'</p>';
-        document.body.appendChild(added_div);
+showSumms(nr_of_reg, suma_sel1, suma_sel2);  
         }else {
-        var added_div = document.createElement("div");
-        added_div.style.fontSize = "14px";
-        added_div.id = "added_div";
-        added_div.innerHTML = '<p>#Of Market Launches: '+nr_of_reg+'</p> <p>Regional Total Amount of Estimated Sales Potential: '+suma_sel1+'</p> <p>Global Total Amount of Estimated Sales Potential: '+parseFloat(suma_sel2).toFixed(2)+'</p>';
-        document.body.appendChild(added_div);
+showSumms(nr_of_reg, suma_sel1, suma_sel2);  
         }
   }
  }
 var container = [];
+
 $(function() {
     var dialog, form,
       title = $( "#title" ),
       brand = $( "#brand" ),
-      win = $( "#win" ).val();
-      timeframe = $( "#timeframe option:selected" ).val();
-      timeframeyear = $( "#timeframeyear" ).val();
+      win = $( "#win" ),
+      timeframe = $( "#timeframe" ),
+      timeframeyear = $( "#timeframeyear" ),
       sales = $( "#sales" ),
       allFields = $( [] ).add( title ).add( brand ).add( sales ),
       tips = $( ".validateTips" ); 
@@ -295,6 +292,7 @@ function checkRegexp( o, regexp, n ) {
     }
     function addUser() {
       var valid = true;
+  
       allFields.removeClass( "ui-state-error" );
       valid = valid && checkLength( title, "title", 3, 16 );
       valid = valid && checkRegexp( title, /^[a-z]([0-9a-z_\s])+$/i, "Title may consist of a-z, 0-9, underscores, spaces and must begin with a letter." );
@@ -304,38 +302,48 @@ function checkRegexp( o, regexp, n ) {
       	var sales2 = parseInt(sales.val())/1.1;
       	suma1 = suma1+parseInt(sales.val());
       	suma2 = suma1/1.1;
-      localStorage.setItem('id['+id+']', id);
-      localStorage.setItem('title['+id+']', title.val());
-      localStorage.setItem('brand['+id+']', brand.val());
-      localStorage.setItem('timeframe['+id+']', timeframe);
-      localStorage.setItem('timeframeyear['+id+']', timeframeyear);
-      localStorage.setItem('sales['+id+']', parseInt(sales.val()));
-      localStorage.setItem('sales2['+id+']', sales2);
+        if (id == 1){
+          var users = new Object();
+        users = {['id'+id] : id, ['title'+id] : title.val(), ['brand'+id] : brand.val(), ['timeframe'+id] : timeframe.val(), ['win'+id] : win.val(), ['timeframeyear'+id] : timeframeyear.val(), ['sales'+id] : parseInt(sales.val())}
+      localStorage.setItem('users', JSON.stringify(users));
+        }
+        else {
+          users = localStorage.getItem('users');
+          users = JSON.parse(users);
+          users['id'+id] = id; users['title'+id] = title.val(); users['brand'+id] = brand.val(); users['timeframe'+id] = timeframe.val(); users['win'+id] = win.val(); users['timeframeyear'+id] = timeframeyear.val(); users['sales'+id] = parseInt(sales.val());
+          localStorage.setItem('users', JSON.stringify(users));
+        }
+    
+      var data_to_array = [];
+
       localStorage.removeItem('nr_max');
       ++nr_max;
       localStorage.setItem('nr_max', nr_max);
-      var test = localStorage.getItem('nr_max');
-   			var element_to_delete = document.getElementById("added_div");
-   			if (element_to_delete)
-   			{
-   				element_to_delete.parentNode.removeChild(element_to_delete);
-   			}
-        $( "#users tbody" ).append( "<tr>" +
-          "<td>ML" + id + "</td>" +
-          "<td>" + title.val() + "</td>" +
-          "<td>" + brand.val() + "</td>" +
-          "<td>" + timeframe + " " +timeframeyear + "</td>" +
-          "<td>" + parseInt(sales.val()) + "</td>" +
-          "<td>" + parseFloat(sales2).toFixed(2) + "</td>" +
-          "<td>To Be Launched</td>" +
-          "<td><img style='cursor:pointer;' onclick='edit_row("+id+")' width='16px' height='16px' id='edit"+id+"' src='css/images/pencil.png'>  <img style='cursor:pointer;' onclick='delete_row("+id+")' width='16px' height='16px' id='delete"+id+"' src='css/images/delete.jpg'></td>" +
-        "</tr>" );
-       ++nr_of_reg;
-        var added_div = document.createElement("div");
-        added_div.style.fontSize = "14px";
-        added_div.id = "added_div";
-        added_div.innerHTML = '<p>#Of Market Launches: '+nr_of_reg+'</p> <p>Regional Total Amount of Estimated Sales Potential: '+suma1+'</p> <p>Global Total Amount of Estimated Sales Potential: '+parseFloat(suma2).toFixed(2)+'</p>';
-        document.body.appendChild(added_div);
+var suma_sel1 = 0; var suma_sel2 = 0;var nr_of_reg = 0;
+  for (i=1;i<=nr_max;i++)
+  {
+    var element_to_delete = document.getElementById("added_div");
+        if (element_to_delete){element_to_delete.parentNode.removeChild(element_to_delete);}
+        title_sel = users['title'+i]; 
+               if (typeof title_sel == 'string'){
+data_to_array.push({"0": users['id'+i], "1" : users['title'+i], "2" : users['brand'+i], "3": users['sales'+i], "4" : (parseInt(users['sales'+i]) / 1.1).toFixed(2), "5" : users['timeframe'+i], "6" : users['timeframeyear'+i]});
+      allData = {"getData" : data_to_array};
+        localStorage.setItem('allData', JSON.stringify(allData));
+
+        mustacheDo();
+
+        sales_sel = users['sales'+i];
+        sales_sel = parseInt(sales_sel);
+        sales2_sel = sales_sel/1.1;
+        suma_sel1 = suma_sel1 + sales_sel;
+        suma_sel2 = suma_sel1/1.1;
+        ++nr_of_reg;
+showSumms(nr_of_reg, suma_sel1, suma_sel2);  
+        }
+        else {
+showSumms(nr_of_reg, suma_sel1, suma_sel2);      
+        }
+  } 
         dialog.dialog( "close" );
       }
       return valid;
@@ -366,3 +374,4 @@ function checkRegexp( o, regexp, n ) {
     });
   });
 });
+
